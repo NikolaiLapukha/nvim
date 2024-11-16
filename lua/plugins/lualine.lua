35a929757573fbedd0c4bfc10348930926a09854
Nikolay Lapukha <nlapukha@yandex.com>
2024-11-16 21:09:28 +0300

    change lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,9 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'onedark',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    theme = 'catppuccin',
+    component_separators = { left = '', right = '|'},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
